16e8bfe72c6b3d339e859721089185fedb7ca708
Robert Barr <robertbarr.uk@gmail.com>
2019-03-13 11:59:54 +0000

    Initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to MessageSocketService!');
+    expect(page.getTitleText()).toEqual('Welcome to realtime-chat-frontend!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'SocketExample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MessageSocketService');
+    expect(app.title).toEqual('realtime-chat-frontend');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MessageSocketService!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to realtime-chat-frontend!');
   });
 });
